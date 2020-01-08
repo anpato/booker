01895c12e813ffd436285d37f4d3a8a1c3f3f0bb
Andre Pato <anpato1994@gmail.com>
2020-01-08 13:12:02 -0500

    added auth and user controller

---
+++ src/controllers/AuthController.js
@@ -0,0 +1,56 @@
+import { Auth } from '../database/Schema'
+
+class AuthController {
+  show = async (req, res) => {
+    try {
+      /*
+      Insert Queries Here
+      */
+    } catch (error) {
+      throw error
+    }
+  }
+
+  getById = async (req, res) => {
+    try {
+      /*
+      Insert Queries Here
+      */
+    } catch (error) {
+      throw error
+    }
+  }
+
+  create = async (req, res) => {
+    try {
+      /*
+      Insert Queries Here
+      */
+    } catch (error) {
+      throw error
+    }
+  }
+
+  destroy = async (req, res) => {
+    try {
+      /*
+      Insert Queries Here
+      */
+    } catch (error) {
+      throw error
+    }
+  }
+
+  update = async (req, res) => {
+    try {
+      /*
+      Insert Queries Here
+      */
+    } catch (error) {
+      throw error
+    }
+  }
+}
+
+export default AuthController
+
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -54,3 +54,59 @@ class UserController {
 
 export default UserController
 
+import { User } from '../database/Schema'
+
+class UserController {
+  show = async (req, res) => {
+    try {
+      /*
+      Insert Queries Here
+      */
+    } catch (error) {
+      throw error
+    }
+  }
+
+  getById = async (req, res) => {
+    try {
+      /*
+      Insert Queries Here
+      */
+    } catch (error) {
+      throw error
+    }
+  }
+
+  create = async (req, res) => {
+    try {
+      /*
+      Insert Queries Here
+      */
+    } catch (error) {
+      throw error
+    }
+  }
+
+  destroy = async (req, res) => {
+    try {
+      /*
+      Insert Queries Here
+      */
+    } catch (error) {
+      throw error
+    }
+  }
+
+  update = async (req, res) => {
+    try {
+      /*
+      Insert Queries Here
+      */
+    } catch (error) {
+      throw error
+    }
+  }
+}
+
+export default UserController
+
