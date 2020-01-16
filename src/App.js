78252ecf4e948a8b8f973cc1a058b32c66798947
Andre Pato <anpato1994@gmail.com>
2020-01-16 16:56:21 -0500

    verify second instance not working witth heroku

--- src/App.js
+++ src/App.js
@@ -3,6 +3,4 @@ import middleWare from './config/ServerConfig'
 import { PORT } from './env'
 
 const app = new Server(PORT, middleWare, '/')
-const app2 = new Server(52000, middleWare, '/v2')
 app.initialize()
-app2.initialize()
